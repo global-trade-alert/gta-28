e3a621056fdc4158cd839d949c00db0430331543
Silvan Hofer <sm.hofer@blueiwn.ch>
2021-08-17 11:30:35 +0200

    First commit

---
+++ code/Taking deliberations/0 data prep Taking deliberations.R
@@ -0,0 +1,9 @@
+# 1. 
+# Go to the WTO website (https://www.wto.org/english/tratop_e/scm_e/scm_e.htm)  and download the statistics for the number of countervailing duty investigations launched each year since 2000. 
+# Please produce a stacked bar chart showing the number of investigations initiated by China, EU28, and USA, and rest of the world. 
+# Please compare this stacked bar chart with a comparable on produced for the years 2009 on using the GTA’s own data (Intervention type “anti-subsidy”; use announcement date for init year; ask Josse to confirm correspondence.). 
+# Hopefully our coverage is better.
+#Author: Silvan
+#Date: 17.8.2021
+#Supervisor Piotr
+
