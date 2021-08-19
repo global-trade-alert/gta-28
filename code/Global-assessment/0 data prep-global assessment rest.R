1a341667bf2a42edf8dcfa5bfe745ccd778208b3
Silvan Hofer <sm.hofer@blueiwn.ch>
2021-08-20 01:15:51 +0200

    finish dataprep for task 6 and 7 of chapter 4

--- code/Global-assessment/0 data prep-global assessment rest.R
+++ code/Global-assessment/0 data prep-global assessment rest.R
@@ -14,17 +14,161 @@ rm(list = ls())
 gta_setwd()
 
 #parameters
+data.path = "0 dev/gta-28-sh/data/Global assessment/"
 
 
-#subsidy.intervention.types <- subset(gtalibrary::int.mast.types, mast.subchapter.id %in% c("L","P7","P8","P9"))$intervention.type
+subsidy.intervention.types <- subset(gtalibrary::int.mast.types, mast.subchapter.id %in% c("L","P7","P8","P9"))$intervention.type
+affects.domestic.markets = subset(gtalibrary::int.mast.types, mast.subchapter.id %in% c("L"))$intervention.type
+affects.foreign.markets = subset(gtalibrary::int.mast.types, mast.subchapter.id %in% c("P7","P8","P9"))$intervention.type
+relevant.intervention.types = list("All subsidy types" = subsidy.intervention.types, "Affects competition in domestic markets" = affects.domestic.markets, "Affects competition in foreign markets" = affects.foreign.markets)
 
-affects.domestic.markets = "L"
-affects.foreign.markets = c("P7","P8","P9")
+eu.countries = gtalibrary::country.names[country.names$is.eu == 1, "name"]
+relevant.juristictions = list("EU" = eu.countries, "USA" = "United States of America", "China" = "China")
 
-eu.countries = countries[countries$is.eu == 1, "name"]
-relevant.juristictions = c(eu.countries, "United States of America", "China")
+
+
+
+#get different goods types
+cpc.goods.codes = gtalibrary::cpc.names[cpc.names$cpc <500 & cpc.names$cpc.digit.level == 3, ]$cpc #get all product cpc codes
+cpc.goods.codes.agricultural = gtalibrary::cpc.names[cpc.names$cpc <100 & cpc.names$cpc.digit.level == 3, ]$cpc
+cpc.goods.codes.not.agricultural = gtalibrary::cpc.names[cpc.names$cpc >= 100 & cpc.names$cpc <500 & cpc.names$cpc.digit.level == 3, ]$cpc
+relevant.goods = list("Only Agricultural" = cpc.goods.codes.agricultural, "Not Agricultural" = cpc.goods.codes.not.agricultural)
+
+
 
 ################################################################################
 #1. get data -------------------------------------------------------------------
 
-gta_trade_coverage()
+
+
+#Task 6 ------------------------------------------------------------------------
+
+
+#initiate dataframe to fill
+data6 = data.frame("Country"  = NA, 
+                   "Subsidy category"  = NA, 
+                   "Importing country"  = NA ,
+                   "Exporting country"  = NA,
+                   "Number of interventions affecting exported product"  = NA,
+                   "Trade coverage estimate for 2009"  = NA,
+                   "Trade coverage estimate for 2010"  = NA,
+                   "Trade coverage estimate for 2011"  = NA,
+                   "Trade coverage estimate for 2012" = NA ,
+                   "Trade coverage estimate for 2013" = NA, 
+                   "Trade coverage estimate for 2014" = NA,
+                   "Trade coverage estimate for 2015" = NA,
+                   "Trade coverage estimate for 2016" = NA,
+                   "Trade coverage estimate for 2017" = NA,                
+                   "Trade coverage estimate for 2018" = NA,
+                   "Trade coverage estimate for 2019" = NA,
+                   "Trade coverage estimate for 2020" = NA,                 
+                   "Trade coverage estimate for 2021" = NA, 
+                   check.names = F)
+  
+
+#to fill the dataframe                 
+line = 0
+
+
+#loop through all combinations of jurisdiction and intervention types 
+for(i in 1:length(relevant.juristictions)){
+  for(y in 1:length(relevant.intervention.types)){
+    
+    line = line + 1 
+    
+    gta_trade_coverage(
+      gta.evaluation = "Red", 
+      cpc.sectors = cpc.goods.codes, 
+      keep.cpc = T, 
+      implementers = relevant.juristictions[[i]], 
+      keep.implementer = T, 
+      intervention.types = relevant.intervention.types[[y]], 
+      keep.type = T
+    )
+    
+    data6 = rbind(data6, cbind("Country" = names(relevant.juristictions)[i],
+                           "Subsidy category" = names(relevant.intervention.types[y]) , 
+                           trade.coverage.estimates[1, ]
+                        )
+                  )
+                  
+   print(line) 
+  }
+
+  line = line + 1
+}
+
+data6 = data6[2:nrow(data6),]
+
+
+
+#Task 7 ------------------------------------------------------------------------
+
+
+#initiate dataframe to fill
+data7 = data.frame("Country"  = NA, 
+                   "Subsidy category"  = NA, 
+                   "Good type" = NA,
+                   "Importing country"  = NA ,
+                   "Exporting country"  = NA,
+                   "Number of interventions affecting exported product"  = NA,
+                   "Trade coverage estimate for 2009"  = NA,
+                   "Trade coverage estimate for 2010"  = NA,
+                   "Trade coverage estimate for 2011"  = NA,
+                   "Trade coverage estimate for 2012" = NA ,
+                   "Trade coverage estimate for 2013" = NA, 
+                   "Trade coverage estimate for 2014" = NA,
+                   "Trade coverage estimate for 2015" = NA,
+                   "Trade coverage estimate for 2016" = NA,
+                   "Trade coverage estimate for 2017" = NA,                
+                   "Trade coverage estimate for 2018" = NA,
+                   "Trade coverage estimate for 2019" = NA,
+                   "Trade coverage estimate for 2020" = NA,                 
+                   "Trade coverage estimate for 2021" = NA, 
+                   check.names = F)
+
+#to fill the dataframe                 
+line = 0
+
+#loop through all combinations of jurisdiction and intervention types 
+for(i in 1:length(relevant.juristictions)) {
+  for (y in 1:length(relevant.intervention.types)) {
+    for (z in 1:length(relevant.goods)) {
+      
+      line = line + 1
+      
+      gta_trade_coverage(
+        gta.evaluation = "Red",
+        cpc.sectors = relevant.goods[[z]],
+        keep.cpc = T,
+        implementers = relevant.juristictions[[i]],
+        keep.implementer = T,
+        intervention.types = relevant.intervention.types[[y]],
+        keep.type = T
+      )
+      
+      
+      data7 = rbind(
+        data7,
+        cbind(
+          "Country" = names(relevant.juristictions)[i],
+          "Subsidy category" = names(relevant.intervention.types[y]) ,
+          "Good type" = names(relevant.goods[z]),
+          trade.coverage.estimates[1,]
+        )
+      )
+      
+      print(line)
+    }
+    
+  }
+  
+}
+#delete unnecesary NA row
+data7 = data7[2:nrow(data7),]
+
+################################################################################
+#2. save data ------------------------------------------------------------------
+
+saveRDS(data6, paste0(data.path, "Task.6.subsidy.trade.share.undifferentiated.RData"))
+saveRDS(data7, paste0(data.path, "Task.7.subsidy.trade.share.differentiated.RData"))
